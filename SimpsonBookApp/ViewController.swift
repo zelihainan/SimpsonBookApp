6289108e273f5d389826ce1d6549c43cfa8c93d3
Zeliha İnan <zelihainann@gmail.com>
2025-08-06 19:31:10 +0300

    Initial Commit

---
+++ SimpsonBookApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpsonBookApp
+//
+//  Created by Zeliha İnan on 6.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
